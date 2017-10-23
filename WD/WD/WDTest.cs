1cbaa2f9df5026e2f6076d824be6540927c65dc1
Алексей Цедрик <ups12211321@outlook.com>
2017-10-23 16:52:52 +0300

    Add WD lab

---
+++ WD/WD/WDTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WD
+{
+    [TestFixture]
+    public class WDTest
+    {
+        [Test]
+        public void oneCanOpenGoogle()
+        {
+            IWebDriver google = new ChromeDriver();
+            google.Navigate().GoToUrl("http://github.com");
+
+            IWebElement buttonSearch = google.FindElement(By.XPath("//a[text()='Sign in']"));
+            buttonSearch.Click();
+
+            IWebElement loginSearch = google.FindElement(By.Name("login"));
+            loginSearch.SendKeys("ups1221@yandex.by");
+
+            IWebElement passwordSearch = google.FindElement(By.Name("password"));
+            passwordSearch.SendKeys("Defender1321");
+
+            IWebElement button = google.FindElement(By.XPath("//input[@value='Sign in']"));
+            button.Click();
+
+        }
+    }
+}
